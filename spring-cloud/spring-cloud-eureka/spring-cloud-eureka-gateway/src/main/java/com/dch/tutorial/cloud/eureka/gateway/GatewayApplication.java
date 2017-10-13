d8bd93c9da17a4fc0a6fd3ad2f796e573f7e4761
davidch93 <david.christianto1@gmail.com>
2017-10-13 13:50:46 +0700

    Update code with Gateway Service

---
+++ spring-cloud/spring-cloud-eureka/spring-cloud-eureka-gateway/src/main/java/com/dch/tutorial/cloud/eureka/gateway/GatewayApplication.java
@@ -0,0 +1,27 @@
+package com.dch.tutorial.cloud.eureka.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Main application of Gateway service and used to start application.
+ * 
+ * @author David.Christianto
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class GatewayApplication {
+
+	/**
+	 * Main method to start application.
+	 * 
+	 * @param args
+	 *            arguments.
+	 */
+	public static void main(String... args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
